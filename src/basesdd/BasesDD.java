83ef2aaadbd9e331548c41a10e1c692a5d13a4f1
Ian <iangraziano18@gmail.com>
2024-06-11 21:28:05 +0200

    Initial commit

---
+++ src/basesdd/BasesDD.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package basesdd;
+
+/**
+ * -Comenta la clase-
+ *
+ * @author IanGz
+ */
+public class BasesDD {
+
+    /**
+     * -Comenta lo que hace el main-
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    
+
+    }
+
+}
